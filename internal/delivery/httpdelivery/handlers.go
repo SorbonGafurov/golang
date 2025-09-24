440d8001e8239db27eb51030c802277073423ebd
Sorbon-Gafurov <s.gafurov.ibt.tj>
2025-09-24 17:15:31 +0500

    last commit

--- internal/delivery/httpdelivery/handlers.go
+++ internal/delivery/httpdelivery/handlers.go
@@ -3,6 +3,7 @@ package httpdelivery
 import (
 	"IbtService/internal/domain"
 	"IbtService/internal/service"
+	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,9 @@ import (
 
 func TestHandler(s service.ExternalService) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		slog.Debug("test")
 		reqData := &domain.Request{}
+
 		if err := c.ShouldBindJSON(reqData); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
 			return
